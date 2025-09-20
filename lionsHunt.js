5e94efba1883ed0055f520cb5aa34e46b7c42454
LakshmiCharitha349 <charithamarthala01@gmail.com>
2025-09-20 17:52:01 +0530

    updation  of lionhunt using loops

--- lionsHunt.js
+++ lionsHunt.js
@@ -1,3 +1,17 @@
 const testCase1 = "LZ";
+let isHunt = -1;
+for(let start = 0;start < testCase1.length; start++) {
+  let hunt = -1;
+  if(testCase1[start] === "L") {
+    for(let search = 0;search < testCase1.length;search++) {
+
+      if(testCase1[search] === "Z") {
+        hunt = search - start;
+        isHunt = hunt === 1 ? 0 : hunt;
+      }
+    }
+  }
+}
+
 console.log("input :",testCase1);
-console.log("output : 0" );
+console.log("output :",isHunt );
