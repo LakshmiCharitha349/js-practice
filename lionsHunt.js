442fa4462b25e2ba40e14fabab88279e21d20dd0
LakshmiCharitha349 <charithamarthala01@gmail.com>
2025-09-20 19:52:43 +0530

    Updating to work for all testcases and rename variable

--- lionsHunt.js
+++ lionsHunt.js
@@ -1,17 +1,22 @@
-const testCase1 = "LZ";
+const forest = "LZ";
 let isHunt = -1;
-for(let start = 0;start < testCase1.length; start++) {
-  let hunt = -1;
-  if(testCase1[start] === "L") {
-    for(let search = 0;search < testCase1.length;search++) {
 
-      if(testCase1[search] === "Z") {
-        hunt = search - start;
-        isHunt = hunt === 1 ? 0 : hunt;
+for(let start = 0;start < forest.length; start++) {
+
+  let hunt = -1;
+  if(forest[start] === "L") { 
+    for(let search = 0;search < forest.length;search++) {
+
+      let temp = isHunt;
+      if(forest[search] === "Z") {
+        hunt = (search - start);
+        isHunt = hunt >= 0 ? hunt - 1 : -(hunt + 1);
+        if(temp < isHunt && temp !== -1) {
+          isHunt = temp;
+        }       
       }
     }
   }
-}
-
-console.log("input :",testCase1);
-console.log("output :",isHunt );
+ }
+console.log("input :",forest);
+console.log("output :",isHunt);
