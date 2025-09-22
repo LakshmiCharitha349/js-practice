cb5c313779e72b6f228560781aefbbf22223d4f2
LakshmiCharitha349 <charithamarthala01@gmail.com>
2025-09-22 09:42:17 +0530

    Update the code using strings

--- tasks/findSubString.js
+++ tasks/findSubString.js
@@ -1,15 +1,26 @@
-let num = 65;
-const subString = 10;
+let num = 21;
+const subString = "101";
+let binaryNum = "";
+
+while( num >= 1) {
+  let str1 = ( num % 2 < 1) ? 0 : 1; 
+  binaryNum = str1 + binaryNum;
+  num = num / 2;
+}
+
+const str = "" + binaryNum;
 let count = 0;
 
-while( num >= 2) {
-  let str1 = ( num % 2 < 1) ? 0 : 1;
-  num = num / 2;
-  let str2 =  ( num % 2 < 1) ? 0 : 1;
-  let finalString = str2 * 10 + str1;
-    
-  if(finalString === subString) {
+for(let index = 0;index <= str.length - subString.length;index++) {
+  let ans = true;
+  for(let start = 0;start < subString.length; start++) {
+    if(str[start + index] !== subString[start]) {
+      ans = false;
+    }
+  }
+  if(ans === true) {
     count = count + 1;
   }
 }
-console.log("NUmber of matches is",count);
+
+console.log("Number of",subString,"'s is",count);
