1a4b1fa0a61b024593d1444ff2cc06cc23066a18
LakshmiCharitha349 <charithamarthala01@gmail.com>
2025-11-03 09:58:11 +0530

    Add encoding functions for numbers and strings

--- bencoder.js
+++ bencoder.js
@@ -2,6 +2,26 @@ function encodeNumbers(data) {
   return "i" + data + "e"; 
 }
 
+function encodeString(data) {
+  const stringSize = data.length;
+
+  return stringSize + ":" + data;
+}
+
+function encode(data) {
+  const typeOfData = typeof data;
+
+  switch(typeOfData) {
+    case "number" :
+      return encodeNumbers(data);
+    case "string" :
+      return encodeString(data);
+    default :
+    return "invalid type";
+  }
+
+}
+
 function displayMessage(data, result, expected, gist) {
   const isPassed = result === expected;
   const resultSymbol = isPassed ? "✅" : "❌";
@@ -17,16 +37,36 @@ function displayMessage(data, result, expected, gist) {
   console.log(message);
 }
 
-function testEncodeNumbers(data, expected, gist) {
-  const result = encodeNumbers(data);
+function testEncode(data, expected, gist) {
+  const result = encode(data);
   displayMessage(data, result, expected, gist);
 }
 
-function testCases() {
-  testEncodeNumbers(123,"i123e","encode a number");
-  testEncodeNumbers(0,"i0e","encode  number 0");
-  testEncodeNumbers(-435,"i-435e","encode negitive number");
-  testEncodeNumbers(45637,"i45637e","encode negitive number");
+function testCasesOnNumbers() {
+  console.log("\n--- ENCODING NUMBERS ---");
+  testEncode(123,"i123e","encode a number");
+  testEncode(0,"i0e","encode  number 0");
+  testEncode(-435,"i-435e","encode negitive number");
+  testEncode(45637,"i45637e","encode negitive number");
 }
 
-testCases();
+function testCasesOnString() {
+  console.log("\n--- ENCODING STRINGS ---");
+  testEncode("string","6:string","encode a string");
+  testEncode("","0:","encode a empty string");
+  testEncode("hello","5:hello","encode a string hello");
+  testEncode("hello","5:hello","encode a string hello");
+  testEncode("hello world","11:hello world","encode a word"); 
+}
+
+function testAllCases() {
+  
+  testCasesOnNumbers();
+  testCasesOnString();
+}
+
+function main() {
+  testAllCases();
+}
+
+main();
