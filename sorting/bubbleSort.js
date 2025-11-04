593e29496eba7bbbc5fb00c3476f66cc55d19872
LakshmiCharitha349 <charithamarthala01@gmail.com>
2025-11-04 14:25:46 +0530

    Enhance bubble sort with benchmarking and iteration count
    Added benchmarking and iteration counting to bubble sort.

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -1,18 +1,43 @@
+let numberOfIterations = 0;
+
 function sort(data) {
   for (let i = 0; i < data.length; i++) {
     for (let j = i + 1; j < data.length; j++) {
-      if(data[i] > data[j]) {
+      numberOfIterations++;
+
+      if (data[i] > data[j]) {
         const temp = data[i];
         data[i] = data[j];
         data[j] = temp;
       }
-      
-    }  
+
+    }
   }
 
   return data;
 }
 
-const data = [4,3,2,1];
-const sortedData = sort(data);
-console.log("Sorted data is", sortedData);
+function randomBtwn(lower, upper) {
+  const randomNumber = Math.floor(Math.random() * (upper - lower));
+
+  return lower + randomNumber;
+}
+
+function benchMark(size) {
+  const data = [];
+  for (let index = 0; index < size; index++) {
+    const element = randomBtwn(1, 100);
+    data.push(element);
+  }
+
+  const sortedData = sort(data);
+  console.log(`${size} -- ${numberOfIterations}`);
+}
+
+function displayData() {
+  console.log("Sorted data is", sortedData);
+  console.log("Numnber Of times :", numberOfIterations);
+
+}
+
+benchMark(10000);
