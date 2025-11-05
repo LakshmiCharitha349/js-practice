830ee27a45fb74e735136192f91f8db3249843ee
LakshmiCharitha349 <charithamarthala01@gmail.com>
2025-11-05 17:48:11 +0530

    Refactor bubble sort for better readability and use

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -1,8 +1,8 @@
-function sort(data,isLessThan) {
+function sort(data, isLessThan) {
   for (let i = 0; i < data.length; i++) {
     for (let j = i + 1; j < data.length; j++) {
 
-      if (isLessThan(data[i],data[j])) {
+      if (isLessThan(data[i], data[j])) {
         const temp = data[i];
         data[i] = data[j];
         data[j] = temp;
@@ -14,15 +14,21 @@ function sort(data,isLessThan) {
   return data;
 }
 
-const isLessThan =  function (element1,element2) {
+const isStringLength = function (element1, element2) {
+  isLessThan(element1.length, element2.length);
+}
+
+const isLessThan = function (element1, element2) {
   return element1 < element2;
 }
 
-const isGreaterThan = function (element1,element2) {
-  return element1 > element2; 
+const isGreaterThan = function (element1, element2) {
+  return element1 > element2;
 }
 
-const data = [9,0,2,1];
+const dataOfNums = [9, 0, 2, 1];
+const dataOfStrings = ["a", "ab", "abc", "abcd"];
 
-console.log("descending order",sort(data, isLessThan));
-console.log("ascending order",sort(data,isGreaterThan));
+console.log("descending order", sort(dataOfNums, isLessThan));
+console.log("ascending order", sort(dataOfNums, isGreaterThan));
+console.log("order of strings", sort(dataOfStrings, isStringLength));
